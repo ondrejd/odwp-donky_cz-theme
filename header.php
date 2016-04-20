36306aa7f6459e21decd64e37b160e57f178ee12
ondrejd <ondrejd@gmail.com>
2016-04-20 11:04:14 +0200

    Updated header and removed redundant icons.

--- header.php
+++ header.php
@@ -13,19 +13,11 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width"><!-- width=device-width, initial-scale=1 -->
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
-	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo( 'template_directory' ); ?>/images/favicon.ico"/>
-	<link rel="fluid-icon" href="<?php bloginfo( 'template_directory' ); ?>/images/icon-512.png" title="<?php _e( 'Donky.cz', 'odwp-donkycz-theme' ); ?>"/>
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo( 'template_directory' ); ?>/images/icon-57.png"/>
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo( 'template_directory' ); ?>/images/icon-72.png"/>
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo( 'template_directory' ); ?>/images/icon-114.png"/>
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo( 'template_directory' ); ?>/images/icon-144.png"/>
-	<meta name="msapplication-TileImage" content="<?php bloginfo( 'template_directory' ); ?>/images/icon-144.png">
-	<meta name="msapplication-TileColor" content="#FFFFFF">
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
